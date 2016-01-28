4d33c0af7bf51b7eec02019eb754e7e475d8a123
brandon schill <brandoschill@gmail.com>
2016-01-28 14:34:06 -0800

    #2 Returned JSON objects for ppg

--- default.js
+++ default.js
@@ -2,5 +2,15 @@ var xhr = new XMLHttpRequest();
 xhr.open('GET', 'http://localhost:1337/dashboard');
 xhr.send();
 xhr.addEventListener('load', function() {
-  console.log(xhr.responseText);
+  var player = document.getElementById('player');
+  var info = document.createTextNode(xhr.responseText);
+  player.appendChild(info);
+  var myPlayers = JSON.parse(xhr.responseText);
+  for(var i = 0; i < 20; i++) {
+    var rppg = (myPlayers[i].FantasyPoints / myPlayers[i].Games);
+    var ppg = rppg.toFixed(2);
+    console.log(ppg);
+  };
+
+  // console.log(xhr.responseText);
 });

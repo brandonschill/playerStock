2029a0161473979d5eacaf92007d9fb7494711b1
brandon schill <brandoschill@gmail.com>
2016-02-01 16:17:39 -0800

    #2 Finalized card format and added photos

--- default.js
+++ default.js
@@ -10,11 +10,12 @@ xhr.addEventListener('load', function() {
   // var info = document.createTextNode(xhr.responseText);
   // player.appendChild(info);
   var myPlayers = JSON.parse(xhr.responseText);
-  for(var i = 0; i < 20; i++) {
+  for(var i = 0; i < myPlayers.length; i++) {
     var rppg = (myPlayers[i].FantasyPoints / myPlayers[i].Games);
     var ppg = rppg.toFixed(2);
     var playerName = myPlayers[i].Name;
-    console.log(ppg, playerName);
+    console.log(myPlayers[i]);
+    buildCard(myPlayers[i]);
   };
   // console.log(JSON.parse(xhr.responseText));
 });
@@ -34,66 +35,128 @@ xhr.addEventListener('load', function() {
 // });
 
 
-// buildCard() output
+var playerPhotos = { 
+  20000571: '/images/lecry.jpg',
+  20000485: '/images/steph.jpg',
+  20001406: '/images/pzinger.jpg',
+  20000544: '/images/harden.jpg',
+  20000442: '/images/wall.png',
+  20000468: '/images/davis.jpg',
+  20000646: '/images/kawhi.jpg',
+  20000664: '/images/paul.jpg',
+  20000742: '/images/george.jpg',
+  20000877: '/images/westbrook.jpg',
+  20000884: '/images/durant.jpg',
+  20000515: '/images/butler.jpg',
+  20001449: '/images/boban.jpg',
+  20000862: '/images/cousins.jpg',
+  20000784: '/images/anthony.jpg',
+  20000705: '/images/kobe.jpg',
+  20000619: '/images/lillard.jpg',
+  20000482: '/images/green.jpg',
+};
 
-// <div class="playerCard">
-//   <div class="row">
-//     <div class="medium-4 columns">
-//       <div class="card">
-//         <div class="image">
-//           <img src="/images/lecry.jpg">
-//           <span class="title">Lebawl James</span>
-//         </div>
-//         <div class="content">
-//           <p><img src="/images/downarrow.png"> Fantasy stock dropped by 2.5 points</p>
-//         </div>
-//         <div class="action">
-//           <p>Season fantasy PPG: 25.6</p>
-//           <p>Previous game fantasy points: 23.1</p>
-//         </div>
-//       </div>
-//     </div>
-//     <div class="medium-6 columns end">
-//       <div class="card">
-//         <div class="content">
-//           <span class="title">Lebron James</span>
-//           <p>LeBron James was tremendously efficient in 31 minutes vs. the Suns, scoring 21 points on 7-of-8 FGs and 7-of-7 FTs, with three rebounds, nine assists and two turnovers.</p>
-//         </div>
-//         <div class="action">
-//           <a href='www.nba.com/playerfile/lebron_james/'>NBA.com</a>
-//           <a href='espn.go.com/nba/player/_/id/1966/lebron-james'>ESPN.com</a>
-//         </div>
-//       </div>
-//     </div>
-//   </div>
-// </div>
 
 function buildCard(data) {
+
+  var rppg = (data.FantasyPoints / data.Games);
+  var ppg = rppg.toFixed(2);
+
   var deck = document.createElement('div');
-  deck.className = "row";
+  deck.className = "playerCard";
+
+  var card = document.createElement('div');
+  card.className = "row";
+  deck.appendChild(card);
 
   var holder = document.createElement('div');
   holder.className = "medium-4 columns";
-  deck.appendChild(holder);
+  card.appendChild(holder);
 
   var outerCard = document.createElement('div');
   outerCard.className = "card";
   holder.appendChild(outerCard);
 
+  var container = document.createElement('div');
+  container.className = "container";
+  outerCard.appendChild(container);
+
+  var cardFront = document.createElement('div');
+  cardFront.className = "front";
+  container.appendChild(cardFront);
+
   var photoHolder = document.createElement('div');
   photoHolder.className = "image";
-  outerCard.appendChild(photoHolder);
+  cardFront.appendChild(photoHolder);
 
   var playerPhoto = document.createElement('img');
-  playerPhoto.setAttribute('src', '/images/lebron.jpg');
+  playerPhoto.setAttribute('src', playerPhotos[data.PlayerID]);
   photoHolder.appendChild(playerPhoto);
 
-  var statHolder = document.createElement('div');
-  statHolder.className = 'content';
-  photoHolder.appendChild(statHolder);
+  var playerTitle = document.createElement('span');
+  playerTitle.className = "title";
+  playerTitle.textContent = data.Name;
+  photoHolder.appendChild(playerTitle);
 
-  var stats = document.createElement('p');
-  var playerStats = document.createTextNode(playerName + ':' + ppg + 'fantasy ppg');
-  statHolder.appendChild(stats);
-  stats.appendChild(playerStats);
-}
+  var stockMove = document.createElement('div');
+  stockMove.className = "content";
+  cardFront.appendChild(stockMove);
+
+  var arrow = document.createElement('img');
+  arrow.setAttribute('src', '/images/arrows.png');
+  stockMove.appendChild(arrow);
+
+  var arrowInfo = document.createTextNode('Fantasy stock ' + 'XXXX ' + 'by ' + 'XXXX ' + 'points');
+  stockMove.appendChild(arrowInfo);
+
+  var stats = document.createElement('div');
+  stats.className = "action";
+  cardFront.appendChild(stats);
+
+  var seasonPpg = document.createElement('p');
+  seasonPpg.textContent = 'Season fantasy PPG: ' + ppg;
+  stats.appendChild(seasonPpg);
+  
+  var recentPoints = document.createElement('p');
+  recentPoints.textContent = 'Previous game fantasy points: ' + 'XXXX';
+  stats.appendChild(recentPoints);
+
+  var cardBack = document.createElement('div');
+  cardBack.className = "back";
+  container.appendChild(cardBack);
+
+  var backContent = document.createElement('div');
+  backContent.className = "content";
+  cardBack.appendChild(backContent);
+
+  var playerInfo = document.createElement('h2');
+  playerInfo.textContent = 'Player Info';
+  backContent.appendChild(playerInfo);
+
+  var playerTitle = document.createElement('span');
+  playerTitle.className = "title";
+  playerTitle.textContent = data.Name;
+  backContent.appendChild(playerTitle);
+
+  var playerNews = document.createElement('p');
+  playerNews.textContent = 'XXXX';
+  backContent.appendChild(playerNews);
+
+  var playerLinks = document.createElement('div');
+  playerLinks.className = "action";
+  cardBack.appendChild(playerLinks);
+
+  var linkOne = document.createElement('a');
+  linkOne.setAttribute('href', 'www.nba.com/playerfile/lebron_james/');
+  linkOne.textContent = 'NBA.com';
+  playerLinks.appendChild(linkOne);
+
+  var linkTwo = document.createElement('a');
+  linkTwo.setAttribute('href', 'espn.go.com/nba/player/_/id/1966/lebron-james');
+  linkTwo.textContent = 'ESPN.com';
+  playerLinks.appendChild(linkTwo);
+
+  var cardHolders = document.getElementById('newCard');
+  cardHolders.appendChild(deck);
+};
+
